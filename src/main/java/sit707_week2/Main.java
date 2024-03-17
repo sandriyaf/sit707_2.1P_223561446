c8f9379ae244cb29daf6ab8d7a89da76e48c2488
sandriyaf <sandriyafernandes10@gmail.com>
2024-03-17 20:51:54 +1100

    Testing a registration page using selenium

---
+++ src/main/java/sit707_week2/Main.java
@@ -0,0 +1,11 @@
+package sit707_week2;
+
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        SeleniumOperations.amazon_registration_page("https://www.amazon.com/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_newcust&prevRID=WBFTRY5P9SD13E62XP5S&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
+    }
+}
